c13ad5abe2bdad677d6204af5974056084eb920c
Davina <davina.houquet@hotmail.com>
2023-03-23 09:05:47 +0100

    Getters et setters de la classe Acteur créés

--- Acteur.php
+++ Acteur.php
@@ -14,6 +14,36 @@ class Acteur{
         $this->_DateNaissance = new DateTime($dateNaissance);
         $this->_Roles = [];
     }
+    public function getNom(){
+        return $this->_Nom;
+    }
+    public function setNom(string $nom){
+        $this->_Nom = $nom;
+    }
+    public function getPrenom(){
+        return $this->_Prenom;
+    }
+    public function setPrenom(string $prenom){
+        $this->_Prenom = $prenom;
+    }
+    public function getSexe(){
+        return $this->_Sexe;
+    }
+    public function setSexe(string $sexe){
+        $this->_Sexe = $sexe;
+    }
+    public function getDateNaissance(){
+        return $this->_DateNaissance;
+    }
+    public function setDateNaissance(DateTime $dateNaisance){
+        $this->_DateNaissance = $dateNaissance;
+    }
+    public function getRoles(){
+        return $this->_Roles;
+    }
+    public function setRoles(string $roles){
+        $this->_Roles = $roles;
+    }
 }
 
 ?>
